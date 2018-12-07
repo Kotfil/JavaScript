db492438431186d3b29031496c985a521c61d8a3
Filip <39651273+Kotfil@users.noreply.github.com>
2018-12-07 17:41:44 +0200

    'NewCode'

---
+++ function.js
@@ -0,0 +1,83 @@
+function barkAtTheMoon() {
+   console.log("Woooooooooooooo!");
+ }
+ barkAtTheMoon();
+
+
+var age =  7;
+function addOne(x){
+  x=x+1;
+}
+addOne(age);
+function addOne(x){
+  x=x+1;
+}
+
+function doIt(param) {
+    param = 2;
+}
+var test = 1;
+doIt(test);
+console.log(test);
+
+
+
+
+
+
+
+dogName = "spike";
+dogWeight = 53;
+if (dogWeight > 20) {
+   console.log(dogName + " says WOOF WOOF");
+} else {
+   console.log(dogName + " says woof woof");
+}
+dogName = "lady";
+dogWeight = 17;
+if (dogWeight > 20) {
+   console.log(dogName + " says WOOF WOOF");
+} else {
+   console.log(dogName + " says woof woof");
+
+
+function bark(name, weight) {
+   if (weight > 20) {
+      console.log(name + " says WOOF WOOF");
+   } else {
+}       console.log(name + " says woof woof");  }
+var student = "krissy";
+var year = 1991;
+var GPA = 381/100;
+var status = "existinguser";
+var isNewUser = (status == "newuser");
+saveMyProfile(student, year, GPA, isNewUser);
+function dogYears(dogName, age) {
+    var years = age * 7;
+    console.log(dogName + " is " + years + " years old");
+}
+var myDog = "Fido";
+dogYears(myDog, 4);
+function makeTea(cups, tea) {
+    console.log("Brewing " + cups + " cups of " + tea);
+}
+var guests = 3;
+makeTea(guests, "Earl Grey");
+function secret() {
+    console.log("The secret of life is 42");
+}
+secret();
+function speak(kind) {
+    var defaultSound = "";
+    if (kind == "dog") {
+        alert("Woof");
+    } else if (kind == "cat") {
+        alert("Meow");
+    } else {
+        alert(defaultSound);
+    }
+}
+var pet = prompt("Enter a type of pet: ");
+speak(pet);
+}
+}
